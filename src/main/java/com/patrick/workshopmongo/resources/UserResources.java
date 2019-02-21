40e2ad8099aa7407ca60dc5ef46a58ed5efa17d2
Patrick Faria <patrickarfaria@gmail.com>
2019-02-21 20:56:34 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/patrick/workshopmongo/resources/UserResources.java
+++ src/main/java/com/patrick/workshopmongo/resources/UserResources.java
@@ -14,7 +14,7 @@ import com.patrick.workshopmongo.services.UserService;
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
-	
+	//private int num;
 	@Autowired
 	private UserService service;
 	

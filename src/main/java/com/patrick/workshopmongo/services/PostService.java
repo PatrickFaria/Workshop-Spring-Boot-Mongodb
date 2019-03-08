72964de7677c39ec6dc13b07ab0ff6de3e5bc86b
Patrick Faria <patrickarfaria@gmail.com>
2019-03-07 22:01:14 -0300

    Consulta com vários critérios

--- src/main/java/com/patrick/workshopmongo/services/PostService.java
+++ src/main/java/com/patrick/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.patrick.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime());
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
